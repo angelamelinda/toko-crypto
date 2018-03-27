e6844a4014c45ee13358d7c0634b5fc91d1f464b
ANGELA\Angela <angela.melinda@gmail.com>
2018-03-27 15:55:26 +0700

    Add Header, Currency, Provider & Store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Home from './containers/Homepage';
+import CurrencyPage from './containers/Currencies';
 import NotFound from './containers/NotFound';
+import Store from './redux/store';
+import Header from './components/Header';
 class App extends Component {
+  componentDidMount() {
+        Promise.resolve()
+        .then(() => {
+            this.setState({ loaded: true })
+        }, (err) => {
+            console.log('An error occurred (but not in setState!)', err);
+        });
+    }
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path='/' component={Home} exact/>
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={Store}>
+        <Router>
+          <div className="App">
+            <Header/>
+            <Switch>
+              <Route path='/' component={Home} exact/>
+              <Route path='/currencies' component={CurrencyPage} exact/>
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
