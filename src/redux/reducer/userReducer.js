0654a6e7ac8530899d0b8cacaa298a88f5b4e6a7
ANGELA\Angela <angela.melinda@gmail.com>
2018-03-28 21:59:34 +0700

    update reducer (add new condition)

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -1,6 +1,6 @@
 import UserConstant from '../constant/constant_user';
 
-const initialState = { isLogging: false, isLoggedIn: false, isRegistering: false, userInformation:{} }
+const initialState = { isLogging: false, isLoggedIn: false, isRegistering: false, userInformation:{}, isFailed:false }
 
 export function User(state = initialState, action) {
   switch (action.type) {
@@ -9,7 +9,7 @@ export function User(state = initialState, action) {
     case UserConstant.USER_LOGIN_SUCCESS:
       return {...state, isLogging: false, isLoggedIn: true, userInformation: action.payLoad}
     case UserConstant.USER_LOGIN_FAILURE:
-      return {...state, isLogging: false, isLoggedIn: false, }
+      return {...state, isLogging: false, isLoggedIn: false, isFailed: true}
 
     case UserConstant.USER_LOGOUT:
       return {...state, isLogging: false, isLoggedIn: false, userInformation:{} }
