d1a9ab23a4241b47c61c6b325d0422be129b03d4
ANGELA\Angela <angela.melinda@gmail.com>
2018-03-28 21:58:22 +0700

    combine reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import { Currency } from './currencyReducer.js';
 import { User } from './userReducer.js';
+import { Balance } from './balanceReducer.js';
 
 const RootReducer = combineReducers({
   Currency,
-  User
+  User,
+  Balance
 })
 
 export default RootReducer;
