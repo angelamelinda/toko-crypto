fd5b20fc3309a81894bda0d42b1e6bd93f540743
ANGELA\Angela <angela.melinda@gmail.com>
2018-03-28 22:14:24 +0700

    Update not found page

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -4,7 +4,14 @@ const Home = (props) => {
   return(
     <div id="not-found">
       <div className="content">
-        <div className="text-center">Page Not Found</div>
+        <div className="container d-flex align-items-center justify-content-center pt-4 pb-4">
+          <div className="text-center">
+            <h1>404</h1>
+            <h3>oops, sorry we can't find that page!</h3>
+            <p>Either something went wrong or the page doesn't exist anymore.</p>
+            <a className="btn btn-lg btn-yellow btn-blue-hover btn-shadow mb-3 mt-3" href="/">Kembali ke depan</a>
+          </div>
+        </div>
       </div>
     </div>
   )
